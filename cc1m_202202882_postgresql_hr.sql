3d8d4c9dfedcc3ce6b7a9fa9f4775b9bb9ed2915
Raquel Rigoni <110852214+raqs-bot@users.noreply.github.com>
2022-11-09 17:30:55 -0300

    Update cc1m_202202882_postgresql_hr.sql

--- cc1m_202202882_postgresql_hr.sql
+++ cc1m_202202882_postgresql_hr.sql
@@ -44,8 +44,7 @@ CREATE TABLE cargos (
 );
 
 COMMENT ON TABLE cargos IS 'Tabela que armazena informações sobre salário e código dos cargos na empresa.';
-COMMENT ON COLUMN cargos.id_cargo IS 'Código de identificação do cargo exercido por determinado empregado.
-Chave estrangeira para a tabela empregados e historico_cargos.';
+COMMENT ON COLUMN cargos.id_cargo IS 'Código de identificação do cargo exercido por determinado empregado. Chave estrangeira para a tabela empregados e historico_cargos.';
 COMMENT ON COLUMN cargos.cargo IS 'Nome do cargo de cada funcionário.';
 COMMENT ON COLUMN cargos.salario_minimo IS 'Salário mínimo admitido ao funcionário de determinado cargo.';
 COMMENT ON COLUMN cargos.salario_maximo IS 'Maior salário admitido a determinado funcionário.';
@@ -56,10 +55,8 @@ CREATE TABLE regioes (
                 nome VARCHAR(25) NOT NULL,
                 CONSTRAINT id_regiao_pk PRIMARY KEY (id_regiao)
 );
-COMMENT ON TABLE regioes IS 'Tabela do tipo 1:N.
-Contém o código de indentificação e nome de cada região cadastrada.';
-COMMENT ON COLUMN regioes.id_regiao IS 'Chave primária da tabela regioes, tipo integer (numero não fracionário).
-Como toda chave primária, essa coluna não pode ser deixada com o valor nulo.';
+COMMENT ON TABLE regioes IS 'Tabela do tipo 1:N. Contém o código de indentificação e nome de cada região cadastrada.';
+COMMENT ON COLUMN regioes.id_regiao IS 'Chave primária da tabela regioes, tipo integer (numero não fracionário). Como toda chave primária, essa coluna não pode ser deixada com o valor nulo.';
 COMMENT ON COLUMN regioes.nome IS 'O nome das regiões estarão armazenadas nessa coluna.';
 
 
@@ -74,11 +71,9 @@ CREATE TABLE paises (
                 CONSTRAINT id_paises_pk PRIMARY KEY (id_pais)
 );
 COMMENT ON TABLE paises IS 'Tabela que informa nome, código de países. Essa tabela contém uma chave primária (id_pais_pk), uma chave estrangeira (id_pais_fk) e nome dos paises.';
-COMMENT ON COLUMN paises.id_pais IS 'Chave primária da tabela países. 
-Essa coluna tem a utilidade de armazenar o código de cada país onde há uma filial/escritório da empresa.';
+COMMENT ON COLUMN paises.id_pais IS 'Chave primária da tabela países. Essa coluna tem a utilidade de armazenar o código de cada país onde há uma filial/escritório da empresa.';
 COMMENT ON COLUMN paises.nome IS 'Nome do país onde se encontra filiais, escritórios, ou departamentos da empresa.';
-COMMENT ON COLUMN paises.id_regiao IS 'Chave primária da tabela regioes, tipo integer (numero não fracionário).
-Como toda chave primária, essa coluna não pode ser deixada com o valor nulo.';
+COMMENT ON COLUMN paises.id_regiao IS 'Chave primária da tabela regioes, tipo integer (numero não fracionário). Como toda chave primária, essa coluna não pode ser deixada com o valor nulo.';
 
 
 CREATE TABLE localizacoes (
@@ -91,14 +86,12 @@ CREATE TABLE localizacoes (
                 CONSTRAINT id_localizacao_pk PRIMARY KEY (id_localizacoes)
 );
 COMMENT ON TABLE localizacoes IS 'Tabela de localizações, contém os endereços de diversos escritórios e facilidades da empresa. Não armazena endereços de clientes.';
-COMMENT ON COLUMN localizacoes.id_localizacoes IS 'Coluna que contém o código de identificação das localizações inseridas na tabela localizacoes. É uma chave primária e por conta disso, não pode ser nula.
-Chave estrangeira para a tabela departamentos.';
+COMMENT ON COLUMN localizacoes.id_localizacoes IS 'Coluna que contém o código de identificação das localizações inseridas na tabela localizacoes. É uma chave primária e por conta disso, não pode ser nula. Chave estrangeira para a tabela departamentos.';
 COMMENT ON COLUMN localizacoes.endereco IS 'Localização por endereço de uma filial, escritório, e/ou departamento da empresa.';
 COMMENT ON COLUMN localizacoes.cep IS 'CEP do endereço/localização de um escritório ou facilidade empresarial.';
 COMMENT ON COLUMN localizacoes.cidade IS 'Cidade onde está localizado o escritório ou outra filial da empresa.';
 COMMENT ON COLUMN localizacoes.uf IS 'Estado (abreviado ou por extenso) onde está localizado o escritório ou outra facilidade da empresa.';
-COMMENT ON COLUMN localizacoes.id_pais IS 'Chave estrangeira que referencia a tabela paises. 
-Essa coluna tem a utilidade de armazenar o código de cada país onde há uma filial/escritório da empresa.';
+COMMENT ON COLUMN localizacoes.id_pais IS 'Chave estrangeira que referencia a tabela paises. Essa coluna tem a utilidade de armazenar o código de cada país onde há uma filial/escritório da empresa.';
 
 
 CREATE UNIQUE INDEX localizacoes_idx
@@ -114,8 +107,7 @@ CREATE TABLE departamentos (
 COMMENT ON TABLE departamentos IS 'Esta tabela reserva informações de identificação (id_departamento, nome, id_localização, id_gerente) dos departamentos da empresa.';
 COMMENT ON COLUMN departamentos.id_departamento IS 'Essa propriedade contém uma chave primária, na qual identifica o departamento cadastrado de acordo com o código de identificação.';
 COMMENT ON COLUMN departamentos.nome IS 'Propriedade contendo o nome do departamento da tabela.';
-COMMENT ON COLUMN departamentos.id_localizacoes IS 'Coluna que contém o código de identificação das localizações inseridas na tabela localizacoes. 
-Chave estrangeira para a tabela departamentos.';
+COMMENT ON COLUMN departamentos.id_localizacoes IS 'Coluna que contém o código de identificação das localizações inseridas na tabela localizacoes. Chave estrangeira para a tabela departamentos.';
 
 
 CREATE TABLE empregados (
@@ -168,15 +160,11 @@ CREATE TABLE historico_cargos (
                 CONSTRAINT data_inicial_pk PRIMARY KEY (id_empregado, data_inicial)
 );
 COMMENT ON TABLE historico_cargos IS 'Essa tabela armazena o histórico de cargos do funcionário. Novas linhas serão adicionadas caso houver mudança de departamento dentro de um cargo ou vice-versa.';
-COMMENT ON COLUMN historico_cargos.id_empregado IS 'Propriedade com valor único na tabela. A chave primária vai garantir que este valor não seja repetido ou confundido com a identificação de outro funcionário.
-Referenciada na tabela histórico_cargo.';
+COMMENT ON COLUMN historico_cargos.id_empregado IS 'Propriedade com valor único na tabela. A chave primária vai garantir que este valor não seja repetido ou confundido com a identificação de outro funcionário. Referenciada na tabela histórico_cargo.';
 COMMENT ON COLUMN historico_cargos.data_inicial IS 'Indica a data de inicio do funcionário em determinado cargo. Essa propriedade deve apresentar valores menores que do que a propriedade data_final.';
-COMMENT ON COLUMN historico_cargos.data_final IS 'Último dia de um funcionário em um determinado cargo. 
-O valor inserido nesta propriedade deve ser maior do que a data inicial.';
-COMMENT ON COLUMN historico_cargos.id_cargo IS 'Código de identificação do último cargo exercido pelo empregado.
-Chave estrangeira (tabela cargos).';
-COMMENT ON COLUMN historico_cargos.id_departamento IS 'Código que identifica o departamento cadastrado de acordo com o código de identificação.
-Corresponde ao último departamento onde o empregado trabalhou.';
+COMMENT ON COLUMN historico_cargos.data_final IS 'Último dia de um funcionário em um determinado cargo. O valor inserido nesta propriedade deve ser maior do que a data inicial.';
+COMMENT ON COLUMN historico_cargos.id_cargo IS 'Código de identificação do último cargo exercido pelo empregado. Chave estrangeira (tabela cargos).';
+COMMENT ON COLUMN historico_cargos.id_departamento IS 'Código que identifica o departamento cadastrado de acordo com o código de identificação. Corresponde ao último departamento onde o empregado trabalhou.';
 
 /*Criação de chave estrangeira nas tabelas*/
 
@@ -258,7 +246,10 @@ ON UPDATE NO ACTION
 NOT DEFERRABLE;
 
 
-/*Implementação de valores nas tabelas na ordem que foream inseridos no postgresql*/
+/*Implementação dos valores nas tabelas criadas anteriormente*/
+
+/*Tais valores devem ser inseridos na ordem apresentada*/
+
 
 /*Valores da tabela regioes*/
 
